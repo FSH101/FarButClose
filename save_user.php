74336805a0bfef5171c057bb1238e9b2bea65313
FSH101 <denispushkin218@gmail.com>
2025-05-13 13:59:25 +0300

    Create save_user.php

---
+++ save_user.php
@@ -0,0 +1,16 @@
+<?php
+$data = json_decode(file_get_contents("php://input"), true);
+if (!$data || !isset($data["user_id"])) {
+  http_response_code(400);
+  exit("Invalid data");
+}
+
+$userId = preg_replace('/[^0-9]/', '', $data["user_id"]);
+$filePath = __DIR__ . "/userdata/user_" . $userId . ".json";
+
+file_put_contents($filePath, json_encode([
+  "partner" => $data["partner"],
+  "date" => $data["date"]
+]));
+
+echo "OK";
