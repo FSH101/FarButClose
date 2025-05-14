75cc3d7243c137defdf66e367bfe7cf735e338a5
FSH101 <denispushkin218@gmail.com>
2025-05-14 11:33:01 +0300

    Delete load_user_data.php

--- load_user_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Пример файла load_user_data.php
-if (file_exists('user_data.json')) {
-    $data = json_decode(file_get_contents('user_data.json'), true);
-    echo json_encode(['success' => true, 'partnerName' => $data['partnerName']]);
-} else {
-    echo json_encode(['success' => false]);
-}
-?>
