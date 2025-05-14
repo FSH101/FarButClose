3cc64b99bf38d8fec67846670ed843a80d527ac6
FSH101 <denispushkin218@gmail.com>
2025-05-14 12:00:55 +0300

    Update load_user.php

--- load_user.php
+++ load_user.php
@@ -1,9 +1,9 @@
 <?php
-$userId = preg_replace('/[^a-zA-Z0-9]/', '', $_GET['userId'] ?? '');
-$file = "data/users/{$userId}.json";
-if (!file_exists($file)) {
-  echo json_encode(['nickname' => '', 'partner' => '', 'startDate' => '', 'coins' => 0, 'chat' => []]);
-  exit;
+$userId = preg_replace('/[^0-9]/', '', $_GET["id"] ?? '');
+$filePath = __DIR__ . "/userdata/user_" . $userId . ".json";
+
+if (file_exists($filePath)) {
+  echo file_get_contents($filePath);
+} else {
+  echo json_encode([]);
 }
-echo file_get_contents($file);
-?>
