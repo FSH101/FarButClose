67e62073c9e63d04ae9271b831e570ebf5e29b37
FSH101 <denispushkin218@gmail.com>
2025-05-14 12:32:14 +0300

    Update load_activity.php

--- load_activity.php
+++ load_activity.php
@@ -1,11 +1,7 @@
 <?php
-$userId = isset($_GET['user_id']) ? preg_replace('/[^0-9]/', '', $_GET['user_id']) : '';
-$filePath = __DIR__ . "/userdata/user_activity_" . $userId . ".json";
-
-if (!$userId || !file_exists($filePath)) {
-  echo json_encode([]);
+$logFile = __DIR__ . "/data/activity_log.json";
+if (!file_exists($logFile)) {
+  echo "[]";
   exit;
 }
-
-$data = file_get_contents($filePath);
-echo $data;
+echo file_get_contents($logFile);
