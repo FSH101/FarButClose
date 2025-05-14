f9f95d065f699de7d6252f1acf0ba567a026884a
FSH101 <denispushkin218@gmail.com>
2025-05-14 11:42:01 +0300

    Delete save_user_data.php

--- save_user_data.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Пример файла save_user_data.php
-$data = json_decode(file_get_contents('php://input'), true);
-file_put_contents('user_data.json', json_encode($data));
-echo json_encode(['success' => true]);
-?>
