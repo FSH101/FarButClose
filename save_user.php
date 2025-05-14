52c7914d361d5866cc6fbde511e5b82d3aa01708
FSH101 <denispushkin218@gmail.com>
2025-05-14 11:50:12 +0300

    Update save_user.php

--- save_user.php
+++ save_user.php
@@ -1,8 +1,14 @@
 <?php
 $data = json_decode(file_get_contents('php://input'), true);
-if (!$data || !isset($data['userId'])) { http_response_code(400); exit; }
-$userId = preg_replace('/[^a-zA-Z0-9]/', '', $data['userId']);
-$file = "data/users/{$userId}.json";
-file_put_contents($file, json_encode($data, JSON_UNESCAPED_UNICODE));
-echo json_encode(['status' => 'ok']);
-?>
+if (!$data || !isset($data['id'])) {
+  http_response_code(400);
+  echo 'Invalid data';
+  exit;
+}
+
+$dir = __DIR__ . '/data/users';
+if (!is_dir($dir)) mkdir($dir, 0777, true);
+
+$userId = preg_replace('/[^a-zA-Z0-9]/', '', $data['id']);
+file_put_contents("$dir/{$userId}.json", json_encode($data));
+echo 'OK';
