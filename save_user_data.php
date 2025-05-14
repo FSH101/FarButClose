67d59dc24fa5b0249233e6aca8fbd7e5b4f2139a
FSH101 <denispushkin218@gmail.com>
2025-05-14 11:12:18 +0300

    Update save_user_data.php

--- save_user_data.php
+++ save_user_data.php
@@ -1,9 +1,6 @@
 <?php
-// Пример файла load_user_data.php
-if (file_exists('user_data.json')) {
-    $data = json_decode(file_get_contents('user_data.json'), true);
-    echo json_encode(['success' => true, 'partnerName' => $data['partnerName']]);
-} else {
-    echo json_encode(['success' => false]);
-}
+// Пример файла save_user_data.php
+$data = json_decode(file_get_contents('php://input'), true);
+file_put_contents('user_data.json', json_encode($data));
+echo json_encode(['success' => true]);
 ?>
