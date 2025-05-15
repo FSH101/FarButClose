d6dd8d8538e15d9aec43d091d0c384727d052b79
FSH101 <denispushkin218@gmail.com>
2025-05-15 11:54:44 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("nav-activity").addEventListener("click", showActivity);
+  document.getElementById("nav-chat").addEventListener("click", showChat);
+  document.getElementById("nav-quest").addEventListener("click", showQuest);
+  document.getElementById("nav-partner").addEventListener("click", showPartner);
+});
